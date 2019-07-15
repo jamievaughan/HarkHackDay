60c9ed66d5927ae95d75e5c78d0d94d00ae4fd11
Matthew Atkin <matthew@harksys.com>
2019-07-15 16:59:20 +0100

    things

--- src/js/API.ts
+++ src/js/API.ts
@@ -2,12 +2,40 @@ const baseUrl = '';
 
 export const getMetricState = async () => {
   const response = await fetch(`${baseUrl}/metric`);
+
+  return response;
 }
 
 export const getDeviceState = async () => {
   const response = await fetch(`${baseUrl}/devices/state`);
+
+  return response;
 }
 
 export const turnDeviceOff = async (device: string) => {
-  const response = await fetch(`${baseUrl}/devices/state`);
+  const body = {isOn: false}
+
+  const response = await fetch(`${baseUrl}/devices/${device}/state`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return response;
+}
+
+export const turnDeviceOn = async (device: string) => {
+  const body = {isOn: true}
+
+  const response = await fetch(`${baseUrl}/devices/${device}/state`, {
+    method: "POST",
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  return response;
 }
